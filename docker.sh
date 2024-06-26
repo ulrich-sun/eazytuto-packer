5a0185314c9a9750963aefefcefc4c48c8465719
ulrich-sun <ulrichnoumsi98@gmail.com>
2024-06-26 01:01:22 +0100

    new codes

--- docker.sh
+++ docker.sh
@@ -9,3 +9,8 @@ sudo systemctl restart docker
 sudo systemctl enable docker
 sudo yum update -y
 sudo yum install  git nano docker-compose-plugin -y
+curl -o /tmp/docker-compose.yml https://raw.githubusercontent.com/ulrich-sun/eazytuto-packer/main/docker-compose.yml
+cd /tmp/
+docker compose up -d
+echo "Your stack it's up and running"
+echo "Good Jobs"
